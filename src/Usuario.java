79aefc2d38097fa94bd0ad87469b4b1101fe92c5
MuriloEvgcs <muriloevgcs@gmail.com>
2024-05-23 16:22:55 -0300

    first commit

---
+++ src/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+        smartTv.aumentarCanal();
+
+
+        System.out.println("Tv ligada ?" + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("Tv ligada ?" + smartTv.ligada);
+    }
+}
